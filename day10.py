ea05dd971e06dfc822c1cd3b65dcce1d3f12fe0c
Jan Bláha <jb790419@gmail.com>
2019-12-10 22:15:20 +0100

    nestihnu to, je to tezky.

--- day10.py
+++ day10.py
@@ -1,17 +1,64 @@
 import day10_input
 import unittest
 
-def cvis(x, y):
-    '''count visible asteroids'''
-    if x == 3 and y == 4:
-        return 8
-    return 0
-
 def part1(data):
     asts = data.splitlines()
     x_l = len(asts[0])
     y_l = len(asts)
-    r = [(x,y,cvis(x,y)) for x in range(x_l) for y in range(y_l) if asts[x][y] == '#']
+    r = []
+    for x in range(x_l):
+        for y in range(y_l):
+            if asts[x][y] == '#':
+                visibles = 0
+                # primy smer sever
+                for n in range(y - 1, -1, -1):
+                    if asts[x][n] == '#':
+                        visibles += 1
+                        break
+                # primy smer jih
+                for n in range(y + 1, y_l):
+                    if asts[x][n] == '#':
+                        visibles += 1
+                        break
+                # primy smer zapad
+                for n in range(x + 1, x_l):
+                    if asts[n][y] == '#':
+                        visibles += 1
+                        break
+                # primy smer vychod
+                for n in range(x - 1, -1, -1):
+                    if asts[n][y] == '#':
+                        visibles += 1
+                        break
+                # I. kvadrant
+                '''
+                ctyri kvadranty
+                I. x v rozmezi x+1 -> x_l step +1
+                   y v rozmezi 0 az y-1
+                '''
+                #for n in range(x + 1, x_l):
+                #    for yy in range(y-1, -1, -1):
+                #        for xx in range(x+1, x_l, n - x + 1):
+                #            if asts[xx][yy] == '#':
+                #                visibles += 1
+                #                break
+                '''
+                II. x v rozmezi x-1 -> 0 step -1
+                    y v rozmezi 0 az y-1
+                '''
+                #for n in range(x-1, -1, -1):
+                #    for yy in range(y-1, -1, -1):
+                #        for xx in range(x-1, -1, 
+                '''
+                III. x v rozmezi x-1 -> 0 step -1
+                     y v rozmezi y+1 -> y_l+1
+                IV. x v rozmezi x+1 -> x_l+1 step +1
+                    y v rozmezi y+1 -> y_l+1
+                pro kazdy asteroid prohledat tyto ctyri kvadranty
+                a pricist asteroidy v primem smeru
+                '''
+                r.append((x, y, visibles))
+
     return sorted(r, key=lambda n: n[2], reverse = True)[0]
 
 class TestMonitor(unittest.TestCase):
