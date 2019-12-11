f1c8cb57257bb440345077b871a4c15a97144294
BLAHA Jan <Jan.BLAHA@cnhind.com>
2019-12-11 14:30:40 +0100

    uz to skoro mam, jeste ozivit sinus, cosinus

--- day10.py
+++ day10.py
@@ -5,38 +5,86 @@ def part1(data):
     asts = data.splitlines()
     x_l = len(asts[0])
     y_l = len(asts)
+    x_range = range(x_l)
+    y_range = range(y_l)
+    points = [(x, y) for y in y_range for x in x_range]
     r = []
-    for x in range(x_l):
-        for y in range(y_l):
-            if asts[x][y] == '#':
+    for (x, y) in points:
+        #y = coord[0]
+        #x = coord[1]
+        if asts[y][x] == '#':
+            lines = {}
+            print(x,y, asts[y][x])
+            '''
+            pro kazdy asteroid vezmu vsechny body site a zjistim uhel
+            potom najdu v tomto uhlu nejblizsi asteroid
+            jak reprezentovat uhel?
+            rozdil mezi x a rozdil mezi y
+            podil
+            '''
+            for (xx, yy) in points:
+                if asts[yy][xx] == '#':
+                #if x != xx and y !=yy:
+                #uhel.append((x-xx, y-yy))
+                    if y!=yy and x!=xx:
+                        uhel = f'{x-xx}:{y-yy}'
+                        try:
+                            nearest = lines[uhel]
+                            if (abs(x - nearest[0]) + abs(y - nearest[1])) > (abs(x -xx) + abs(y-yy)):
+                                lines[uhel] = (xx, yy)
+                        except KeyError:
+                            lines[uhel] = (xx, yy)
+                    #  + vyresit situaci na ose y=yy x=xx a je to!
+            print(lines)
+
+def part1x(data):
+    asts = data.splitlines()
+    x_l = len(asts[0])
+    y_l = len(asts)
+    r = []
+    for y in range(y_l):
+        for x in range(x_l):
+            print(x,y, asts[y][x])
+            if asts[y][x] == '#':
                 visibles = 0
                 # primy smer sever
-                for n in range(y - 1, -1, -1):
-                    if asts[x][n] == '#':
+                for yy in range(y - 1, -1, -1):
+                    if asts[yy][x] == '#':
                         visibles += 1
                         break
                 # primy smer jih
-                for n in range(y + 1, y_l):
-                    if asts[x][n] == '#':
+                for yy in range(y + 1, y_l):
+                    if asts[yy][x] == '#':
                         visibles += 1
                         break
                 # primy smer zapad
                 for n in range(x + 1, x_l):
-                    if asts[n][y] == '#':
+                    if asts[y][n] == '#':
                         visibles += 1
                         break
                 # primy smer vychod
                 for n in range(x - 1, -1, -1):
-                    if asts[n][y] == '#':
+                    if asts[y][n] == '#':
                         visibles += 1
                         break
                 # I. kvadrant
                 '''
                 ctyri kvadranty
+                ... resp osm oktantu, jak vychazi najevo
                 I. x v rozmezi x+1 -> x_l step +1
                    y v rozmezi 0 az y-1
                 '''
-                #for n in range(x + 1, x_l):
+                if y > 0:
+                    for n in range(x + 1, x_l):
+                        # jak daleko je n od x - to je step dalsi iterace
+                        #print(n, x, x_l)
+                        #print(list(range(n, x_l, n-x)))
+
+                        line = [(xx, yy) for xx in range(n, x_l, n-x) for yy in range(y-1, -1, -1)]
+                        print(x, y, n,line)
+
+
+                    
                 #    for yy in range(y-1, -1, -1):
                 #        for xx in range(x+1, x_l, n - x + 1):
                 #            if asts[xx][yy] == '#':
