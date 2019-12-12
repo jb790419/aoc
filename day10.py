0619ec2abee6a02f0b55e02b860a2f47d1e665a8
Jan Bláha <jb790419@gmail.com>
2019-12-12 02:30:27 +0100

    day10 finished

--- day10.py
+++ day10.py
@@ -1,6 +1,8 @@
 import day10_input
 import unittest
 
+from math import atan2, degrees, pi
+
 def part1(data):
     asts = data.splitlines()
     x_l = len(asts[0])
@@ -14,100 +16,62 @@ def part1(data):
         #x = coord[1]
         if asts[y][x] == '#':
             lines = {}
-            print(x,y, asts[y][x])
             '''
             pro kazdy asteroid vezmu vsechny body site a zjistim uhel
             potom najdu v tomto uhlu nejblizsi asteroid
             jak reprezentovat uhel?
-            rozdil mezi x a rozdil mezi y
-            podil
+            trigonometric functions to the rescue!
             '''
             for (xx, yy) in points:
                 if asts[yy][xx] == '#':
-                #if x != xx and y !=yy:
-                #uhel.append((x-xx, y-yy))
-                    if y!=yy and x!=xx:
-                        uhel = f'{x-xx}:{y-yy}'
-                        try:
-                            nearest = lines[uhel]
-                            if (abs(x - nearest[0]) + abs(y - nearest[1])) > (abs(x -xx) + abs(y-yy)):
-                                lines[uhel] = (xx, yy)
-                        except KeyError:
+                    if x == xx and y == yy:
+                        continue
+                    uhel = degrees(atan2(x-xx, y-yy))
+                    try:
+                        nearest = lines[uhel]
+                        if (abs(x - nearest[0]) + abs(y - nearest[1])) > (abs(x-xx) + abs(y-yy)):
                             lines[uhel] = (xx, yy)
-                    #  + vyresit situaci na ose y=yy x=xx a je to!
-            print(lines)
+                    except KeyError:
+                        lines[uhel] = (xx, yy)
+            r.append((x, y, len(lines.keys())))
+    return sorted(r, key=lambda n: n[2], reverse = True)[0]
 
-def part1x(data):
+def part2(data, myx, myy):
     asts = data.splitlines()
     x_l = len(asts[0])
     y_l = len(asts)
+    x_range = range(x_l)
+    y_range = range(y_l)
+    points = [(x, y) for y in y_range for x in x_range]
+    lines = {}
+    for (xx, yy) in points:
+        if asts[yy][xx] == '#':
+            if myx == xx and myy == yy:
+                continue
+            arc = atan2(myx-xx, myy-yy)
+            arc = arc if arc > 0 else arc + 2*pi
+            arc = degrees(arc)
+            try:
+                lines[arc].append((xx,yy))
+            except KeyError:
+                lines[arc] = []
+                lines[arc].append((xx, yy))
+    for arc, line in lines.items():
+        lines[arc] = sorted(lines[arc], key=lambda n: abs(myx-n[0])+abs(myy-n[1]))
+
+
     r = []
-    for y in range(y_l):
-        for x in range(x_l):
-            print(x,y, asts[y][x])
-            if asts[y][x] == '#':
-                visibles = 0
-                # primy smer sever
-                for yy in range(y - 1, -1, -1):
-                    if asts[yy][x] == '#':
-                        visibles += 1
-                        break
-                # primy smer jih
-                for yy in range(y + 1, y_l):
-                    if asts[yy][x] == '#':
-                        visibles += 1
-                        break
-                # primy smer zapad
-                for n in range(x + 1, x_l):
-                    if asts[y][n] == '#':
-                        visibles += 1
-                        break
-                # primy smer vychod
-                for n in range(x - 1, -1, -1):
-                    if asts[y][n] == '#':
-                        visibles += 1
-                        break
-                # I. kvadrant
-                '''
-                ctyri kvadranty
-                ... resp osm oktantu, jak vychazi najevo
-                I. x v rozmezi x+1 -> x_l step +1
-                   y v rozmezi 0 az y-1
-                '''
-                if y > 0:
-                    for n in range(x + 1, x_l):
-                        # jak daleko je n od x - to je step dalsi iterace
-                        #print(n, x, x_l)
-                        #print(list(range(n, x_l, n-x)))
-
-                        line = [(xx, yy) for xx in range(n, x_l, n-x) for yy in range(y-1, -1, -1)]
-                        print(x, y, n,line)
+    while len(lines.keys()) > 0:
+        for arc in sorted(lines.keys(), reverse = True):
+            r.append(lines[arc].pop(0))
+            if len(lines[arc]) == 0:
+                del(lines[arc])
+    return r
 
 
-                    
-                #    for yy in range(y-1, -1, -1):
-                #        for xx in range(x+1, x_l, n - x + 1):
-                #            if asts[xx][yy] == '#':
-                #                visibles += 1
-                #                break
-                '''
-                II. x v rozmezi x-1 -> 0 step -1
-                    y v rozmezi 0 az y-1
-                '''
-                #for n in range(x-1, -1, -1):
-                #    for yy in range(y-1, -1, -1):
-                #        for xx in range(x-1, -1, 
-                '''
-                III. x v rozmezi x-1 -> 0 step -1
-                     y v rozmezi y+1 -> y_l+1
-                IV. x v rozmezi x+1 -> x_l+1 step +1
-                    y v rozmezi y+1 -> y_l+1
-                pro kazdy asteroid prohledat tyto ctyri kvadranty
-                a pricist asteroidy v primem smeru
-                '''
-                r.append((x, y, visibles))
 
-    return sorted(r, key=lambda n: n[2], reverse = True)[0]
+
+
 
 class TestMonitor(unittest.TestCase):
     def test_part1_1(self):
@@ -120,6 +84,22 @@ class TestMonitor(unittest.TestCase):
         self.assertEqual(part1(day10_input.test4_data), (6, 3, 41))
     def test_part1_5(self):
         self.assertEqual(part1(day10_input.test5_data), (11, 13, 210))
+    def test_part2_5(self):
+        poradi = part2(day10_input.test5_data, 11, 13)
+        self.assertEqual(poradi[0], (11,12))
+        self.assertEqual(poradi[1], (12,1))
+        self.assertEqual(poradi[2], (12,2))
+        self.assertEqual(poradi[9], (12,8))
+        self.assertEqual(poradi[19], (16,0))
+        self.assertEqual(poradi[49], (16,9))
+        self.assertEqual(poradi[99], (10,16))
+        self.assertEqual(poradi[198], (9,6))
+        self.assertEqual(poradi[199], (8,2))
+        self.assertEqual(poradi[200], (10,9))
+        self.assertEqual(poradi[298], (11,1))
 
 if __name__ == '__main__':
-    print(part1(day10_input.input_data))
+    lookup = part1(day10_input.input_data)
+    print(lookup)
+    poradi = part2(day10_input.input_data, lookup[0], lookup[1])
+    print(poradi[199][0]*100 + poradi[199][1])
