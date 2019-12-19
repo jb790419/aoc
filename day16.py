2177cb27a3d16e4c8242c5a7224a2cb1137bb2bc
Jan Bláha <jb790419@gmail.com>
2019-12-19 22:47:35 +0100

    day16 part2 copied from reddit

--- day16.py
+++ day16.py
@@ -61,29 +61,55 @@ def signal(instr, phase=0):
             f = int(str(sum([x*y for x, y in zip(inpl, d)]))[-1])
             pl.append(f)
         inp.append(pl)
-    return pl[:8]
+    return pl
+
+def first8(instr, phase = 0):
+
+    return signal(instr, phase = phase)[:8]
+
+def tenkilo(s):
+    from itertools import cycle, accumulate
+    offset = int(s[:7])
+    digits = [int(i) for i in s]
+    # If `rep` is `digits` repeated 10K times, construct: 
+    #     arr = [rep[-1], rep[-2], ..., rep[offset]]
+    l = 10000 * len(digits) - offset
+    i = cycle(reversed(digits))
+    arr = [next(i) for _ in range(l)]
+    # Repeatedly take the partial sums mod 10
+    for _ in range(100):
+        arr = [n % 10 for n in accumulate(arr)]
+    return "".join(str(i) for i in arr[-1:-9:-1])
+
 
 class TestSignal(unittest.TestCase):
     def test_phase1(self):
-        self.assertEqual(signal('12345678', phase=1), [4,8,2,2,6,1,5,8])
+        self.assertEqual(first8('12345678', phase=1), [4,8,2,2,6,1,5,8])
     def test_phase2(self):
-        self.assertEqual(signal('12345678', phase=2), [3,4,0,4,0,4,3,8])
+        self.assertEqual(first8('12345678', phase=2), [3,4,0,4,0,4,3,8])
     def test_phase3(self):
-        self.assertEqual(signal('12345678', phase=3), [0,3,4,1,5,5,1,8])
+        self.assertEqual(first8('12345678', phase=3), [0,3,4,1,5,5,1,8])
     def test_phase4(self):
-        self.assertEqual(signal('12345678', phase=4), [0,1,0,2,9,4,9,8])
+        self.assertEqual(first8('12345678', phase=4), [0,1,0,2,9,4,9,8])
 
     def test_large1(self):
-        self.assertEqual(signal('80871224585914546619083218645595', phase=100), [2,4,1,7,6,1,7,6])
+        self.assertEqual(first8('80871224585914546619083218645595', phase=100), [2,4,1,7,6,1,7,6])
 
     def test_large2(self):
-        self.assertEqual(signal('19617804207202209144916044189917', phase=100), [7,3,7,4,5,4,1,8])
+        self.assertEqual(first8('19617804207202209144916044189917', phase=100), [7,3,7,4,5,4,1,8])
 
     def test_large3(self):
-        self.assertEqual(signal('69317163492948606335995924319873', phase=100), [5,2,4,3,2,1,3,3])
+        self.assertEqual(first8('69317163492948606335995924319873', phase=100), [5,2,4,3,2,1,3,3])
+
+    def test_part2(self):
+        self.assertEqual(tenkilo('03036732577212944063491565474664'), '84462026')
+        self.assertEqual(tenkilo('02935109699940807407585447034323'), '78725270')
+        self.assertEqual(tenkilo('03081770884921959731165446850517'), '53553731')
+
 
 
 if __name__ == '__main__':
 
     input_data = '59750939545604170490448806904053996019334767199634549908834775721405739596861952646254979483184471162036292390420794027064363954885147560867913605882489622487048479055396272724159301464058399346811328233322326527416513041769256881220146486963575598109803656565965629866620042497176335792972212552985666620566167342140228123108131419565738662203188342087202064894410035696740418174710212851654722274533332525489527010152875822730659946962403568074408253218880547715921491803133272403027533886903982268040703808320401476923037465500423410637688454817997420944672193747192363987753459196311580461975618629750912028908140713295213305315022251918307904937'
-    print(signal(input_data, phase=100))
+    #print(signal(input_data, phase=100))
+    print(tenkilo(input_data))
